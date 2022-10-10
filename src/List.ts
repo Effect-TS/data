42683ad07ea98c9fa3e4feb46e28429c9d53151d
Michael Arnaldi <arnaldimichael@gmail.com>
2022-10-10 21:53:11 +0000

    begin porting of List

---
+++ src/List.ts
@@ -0,0 +1,326 @@
+/**
+ * @since 1.0.0
+ */
+
+import type * as HKT from "@fp-ts/core/HKT"
+import type { Option } from "@fp-ts/core/Option"
+import type { Predicate } from "@fp-ts/core/Predicate"
+import type { Refinement } from "@fp-ts/core/Refinement"
+import type { Result } from "@fp-ts/core/Result"
+import type * as _apply from "@fp-ts/core/typeclasses/Apply"
+import type * as _fromIdentity from "@fp-ts/core/typeclasses/FromIdentity"
+import type * as _functor from "@fp-ts/core/typeclasses/Functor"
+import * as LI from "@fp-ts/data/internal/List"
+import type * as L from "@fp-ts/data/List"
+
+/**
+ * @since 1.0.0
+ */
+export declare namespace List {
+  export type Cons<A> = L.Cons<A>
+  export type Nil<A> = L.Nil<A>
+}
+
+/**
+ * @since 1.0.0
+ *
+ * @category model
+ */
+export interface ListBuilder<A> extends Iterable<A> {
+  readonly length: number
+
+  readonly isEmpty: () => boolean
+
+  readonly unsafeHead: () => A
+
+  readonly unsafeTail: () => List<A>
+
+  readonly append: (elem: A) => ListBuilder<A>
+
+  readonly prepend: (elem: A) => ListBuilder<A>
+
+  readonly unprepend: (this: this) => A
+
+  readonly build: () => List<A>
+
+  readonly insert: (idx: number, elem: A) => ListBuilder<A>
+
+  readonly reduce: <B>(b: B, f: (b: B, a: A) => B) => B
+}
+
+/**
+ * @since 1.0.0
+ * @category symbol
+ */
+export const ListTypeId: unique symbol = LI.ListTypeId as ListTypeId
+
+/**
+ * @since 1.0.0
+ * @category symbol
+ */
+export type ListTypeId = typeof ListTypeId
+
+/**
+ * @since 1.0.0
+ * @category model
+ */
+export interface Cons<A> extends Iterable<A> {
+  readonly _typeId: ListTypeId
+  readonly _tag: "Cons"
+  readonly _A: (_: never) => A
+  readonly head: A
+  readonly tail: List<A>
+}
+
+/**
+ * @since 1.0.0
+ * @category model
+ */
+export interface Nil<A> extends Iterable<A> {
+  readonly _typeId: ListTypeId
+  readonly _tag: "Nil"
+  readonly _A: (_: never) => A
+}
+
+/**
+ * @since 1.0.0
+ * @category model
+ */
+export type List<A> = Cons<A> | Nil<A>
+
+/**
+ * @category type lambdas
+ * @since 3.0.0
+ */
+export interface ListTypeLambda extends HKT.TypeLambda {
+  readonly type: List<this["Out1"]>
+}
+
+/**
+ * @since 1.0.0
+ * @category constructors
+ */
+export const builder: <A>() => ListBuilder<A> = LI.builder
+
+/**
+ * @since 1.0.0
+ * @category constructors
+ */
+export const make: <As extends ReadonlyArray<any>>(...prefix: As) => List<As[number]> = LI.make
+
+/**
+ * @since 1.0.0
+ * @category constructors
+ */
+export const of: <A>(a: A) => List<A> = LI.of
+
+/**
+ * @since 1.0.0
+ * @category constructors
+ */
+export const cons: <A>(head: A, tail: List<A>) => Cons<A> = LI.cons
+
+/**
+ * @since 1.0.0
+ * @category constructors
+ */
+export const nil: <A = never>() => Nil<A> = LI.nil
+
+/**
+ * @since 1.0.0
+ * @category filtering
+ */
+export const drop: (n: number) => <A>(self: List<A>) => List<A> = LI.drop
+
+/**
+ * @since 1.0.0
+ * @category filtering
+ */
+export const take: (n: number) => <A>(self: List<A>) => List<A> = LI.take
+
+/**
+ * @since 1.0.0
+ * @category filtering
+ */
+export const filter: {
+  <A, B extends A>(p: Refinement<A, B>): (self: List<A>) => List<B>
+  <A>(p: Predicate<A>): (self: List<A>) => List<A>
+} = LI.filter
+
+/**
+ * @since 1.0.0
+ * @category refinements
+ */
+export const isList: {
+  <A>(u: Iterable<A>): u is List<A>
+  (u: unknown): u is List<unknown>
+} = LI.isList
+
+/**
+ * @since 1.0.0
+ * @category refinements
+ */
+export const isCons: <A>(self: List<A>) => self is Cons<A> = LI.isCons
+
+/**
+ * @since 1.0.0
+ * @category refinements
+ */
+export const isNil: <A>(self: List<A>) => self is Nil<A> = LI.isNil
+
+/**
+ * @since 1.0.0
+ * @category mutations
+ */
+export const prepend: <B>(elem: B) => <A>(self: List<A>) => Cons<A | B> = LI.prepend
+
+/**
+ * @since 1.0.0
+ * @category mutations
+ */
+export const prependAll: <B>(prefix: List<B>) => <A>(self: List<A>) => List<A | B> = LI.prependAll
+
+/**
+ * @since 1.0.0
+ * @category mutations
+ */
+export const concat: <B>(prefix: List<B>) => <A>(self: List<A>) => List<A | B> = LI.concat
+
+/**
+ * @since 1.0.0
+ * @category mutations
+ */
+export const reverse: <A>(self: List<A>) => List<A> = LI.reverse
+
+/**
+ * @since 1.0.0
+ * @category partitioning
+ */
+export const partition: <A>(
+  f: Predicate<A>
+) => (self: List<A>) => readonly [List<A>, List<A>] = LI.partition
+
+/**
+ * @since 1.0.0
+ * @category partitioning
+ */
+export const partitionMap: <A, B, C>(
+  f: (a: A) => Result<B, C>
+) => (self: List<A>) => readonly [List<B>, List<C>] = LI.partitionMap
+
+/**
+ * @since 1.0.0
+ * @category partitioning
+ */
+export const splitAt: (n: number) => <A>(self: List<A>) => readonly [List<A>, List<A>] = LI.splitAt
+
+/**
+ * @since 1.0.0
+ * @category getters
+ */
+export const head: <A>(self: List<A>) => Option<A> = LI.head
+
+/**
+ * @since 1.0.0
+ * @category getters
+ */
+export const tail: <A>(self: List<A>) => Option<List<A>> = LI.tail
+
+/**
+ * @since 1.0.0
+ * @category constructors
+ */
+export const empty: <A = never>() => List<A> = LI.empty
+
+/**
+ * @since 1.0.0
+ * @category predicates
+ */
+export const any: <A>(p: Predicate<A>) => (self: List<A>) => boolean = LI.any
+
+/**
+ * @since 1.0.0
+ * @category predicates
+ */
+export const all: <A>(p: Predicate<A>) => (self: List<A>) => boolean = LI.all
+
+/**
+ * @since 1.0.0
+ * @category finding
+ */
+export const find: {
+  <A, B extends A>(p: Refinement<A, B>): (self: List<A>) => Option<B>
+  <A>(p: Predicate<A>): (self: List<A>) => Option<A>
+} = LI.find
+
+/**
+ * @since 1.0.0
+ * @category traversing
+ */
+export const forEach: <A, U>(f: (a: A) => U) => (self: List<A>) => void = LI.forEach
+
+/**
+ * @since 1.0.0
+ * @category sequencing
+ */
+export const flatMap: <A, B>(f: (a: A) => List<B>) => (self: List<A>) => List<B> = LI.flatMap
+
+/**
+ * @since 1.0.0
+ * @category mapping
+ */
+export const map: <A, B>(f: (a: A) => B) => (self: List<A>) => List<B> = LI.map
+
+/**
+ * @since 1.0.0
+ * @category conversions
+ */
+export const fromIterable: <A>(prefix: Iterable<A>) => List<A> = LI.from
+
+/**
+ * @since 1.0.0
+ * @category folding
+ */
+export const reduce: <A, B>(b: B, f: (b: B, a: A) => B) => (self: List<A>) => B = LI.reduce
+
+/**
+ * @since 1.0.0
+ * @category apply
+ */
+export const ap: <A>(fa: List<A>) => <B>(self: List<(a: A) => B>) => List<B> = LI.ap
+
+/**
+ * @since 1.0.0
+ * @category unsafe
+ */
+export const headUnsafe: <A>(self: List<A>) => A = LI.unsafeHead
+
+/**
+ * @since 1.0.0
+ * @category unsafe
+ */
+export const tailUnsafe: <A>(self: List<A>) => List<A> = LI.unsafeTail
+
+/**
+ * @since 1.0.0
+ * @category unsafe
+ */
+export const lastUnsafe: <A>(self: List<A>) => A = LI.unsafeLast
+
+/**
+ * @since 1.0.0
+ * @category instances
+ */
+export const Functor: _functor.Functor<ListTypeLambda> = LI.Functor
+
+/**
+ * @since 1.0.0
+ * @category instances
+ */
+export const FromIdentity: _fromIdentity.FromIdentity<ListTypeLambda> = LI.FromIdentity
+
+/**
+ * @since 1.0.0
+ * @category instances
+ */
+export const Apply: _apply.Apply<ListTypeLambda> = LI.Apply
---
+++ src/internal/List.ts
@@ -0,0 +1,721 @@
+/*
+ * This file is ported from
+ *
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ */
+import { pipe, unsafeCoerce } from "@fp-ts/core/Function"
+import * as O from "@fp-ts/core/Option"
+import type { Predicate } from "@fp-ts/core/Predicate"
+import type { Refinement } from "@fp-ts/core/Refinement"
+import * as R from "@fp-ts/core/Result"
+import type * as L from "@fp-ts/data/List"
+
+import type * as _apply from "@fp-ts/core/typeclasses/Apply"
+import type * as _fromIdentity from "@fp-ts/core/typeclasses/FromIdentity"
+import type * as _functor from "@fp-ts/core/typeclasses/Functor"
+import type { Ord } from "@fp-ts/core/typeclasses/Ord"
+
+/** @internal */
+export const ListTypeId: L.ListTypeId = Symbol.for("@fp-ts/data/List") as L.ListTypeId
+
+/** @internal */
+export class ConsImpl<A> implements Iterable<A>, L.Cons<A> {
+  readonly _tag = "Cons"
+  readonly _A: (_: never) => A = (_) => _
+  readonly _typeId: L.ListTypeId = ListTypeId
+  constructor(readonly head: A, public tail: L.List<A>) {}
+  [Symbol.iterator](): Iterator<A> {
+    let done = false
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    let these: L.List<A> = this
+    return {
+      next() {
+        if (done) {
+          return this.return!()
+        }
+        if (these._tag === "Nil") {
+          done = true
+          return this.return!()
+        }
+        const value: A = these.head
+        these = these.tail
+        return { done, value }
+      },
+      return(value?: unknown) {
+        if (!done) {
+          done = true
+        }
+        return { done: true, value }
+      }
+    }
+  }
+}
+
+/** @internal */
+export class NilImpl<A> implements Iterable<A>, L.Nil<A> {
+  readonly _tag = "Nil"
+  readonly _A: (_: never) => A = (_) => _
+  readonly _typeId: L.ListTypeId = ListTypeId;
+
+  [Symbol.iterator](): Iterator<A> {
+    return {
+      next() {
+        return { done: true, value: undefined }
+      }
+    }
+  }
+}
+
+/** @internal */
+export const _Nil: L.Nil<never> = new NilImpl<never>()
+
+/** @internal */
+export function nil<A = never>(): L.Nil<A> {
+  return _Nil
+}
+
+/** @internal */
+export function cons<A>(head: A, tail: L.List<A>): L.Cons<A> {
+  return new ConsImpl(head, tail)
+}
+
+/** @internal */
+export function isNil<A>(self: L.List<A>): self is L.Nil<A> {
+  return self._tag === "Nil"
+}
+
+/** @internal */
+export function isCons<A>(self: L.List<A>): self is L.Cons<A> {
+  return self._tag === "Cons"
+}
+
+/** @internal */
+export function length<A>(self: L.List<A>): number {
+  let these = self
+  let len = 0
+  while (!isNil(these)) {
+    len += 1
+    these = these.tail
+  }
+  return len
+}
+
+/** @internal */
+export function equalsWith<A, B>(
+  that: L.List<B>,
+  f: (a: A, b: B) => boolean
+) {
+  return (self: L.List<A>): boolean => {
+    if ((self as L.List<A | B>) === that) {
+      return true
+    } else if (length(self) !== length(that)) {
+      return false
+    } else {
+      const i0 = self[Symbol.iterator]()
+      const i1 = that[Symbol.iterator]()
+      let a: IteratorResult<A>
+      let b: IteratorResult<B>
+      while (!(a = i0.next()).done && !(b = i1.next()).done) {
+        if (!f(a.value, b.value)) {
+          return false
+        }
+      }
+      return true
+    }
+  }
+}
+
+/** @internal */
+export function isList<A>(u: Iterable<A>): u is L.List<A>
+/** @internal */
+export function isList(u: unknown): u is L.List<unknown>
+/** @internal */
+export function isList(u: unknown): u is L.List<unknown> {
+  return typeof u === "object" && u != null && ListTypeId in u
+}
+
+/** @internal */
+export function reduce<A, B>(b: B, f: (b: B, a: A) => B): (self: L.List<A>) => B {
+  return (self) => {
+    let acc = b
+    let these = self
+    while (!isNil(these)) {
+      acc = f(acc, these.head)
+      these = these.tail
+    }
+    return acc
+  }
+}
+
+/** @internal */
+export function unsafeHead<A>(self: L.List<A>): A {
+  if (isNil(self)) {
+    throw new Error("Expected List to be not empty")
+  }
+  return self.head
+}
+
+/** @internal */
+export function unsafeTail<A>(self: L.List<A>): L.List<A> {
+  if (isNil(self)) {
+    throw new Error("Expected List to be not empty")
+  }
+  return self.tail
+}
+
+/** @internal */
+export function drop(n: number) {
+  return <A>(self: L.List<A>): L.List<A> => {
+    if (n <= 0) {
+      return self
+    }
+
+    if (n >= length(self)) {
+      return nil<A>()
+    }
+
+    let these = self
+
+    let i = 0
+    while (!isNil(these) && i < n) {
+      these = these.tail
+
+      i += 1
+    }
+
+    return these
+  }
+}
+
+/** @internal */
+export function take(n: number) {
+  return <A>(self: L.List<A>): L.List<A> => {
+    if (n <= 0) {
+      return self
+    }
+
+    if (n >= length(self)) {
+      return self
+    }
+
+    let these = make(unsafeHead(self))
+
+    let i = 0
+    while (!isNil(these) && i < n) {
+      these = pipe(these, prependAll(make(unsafeHead(self))))
+
+      i += 1
+    }
+
+    return these
+  }
+}
+
+/** @internal */
+export function prependAll<B>(prefix: L.List<B>) {
+  return <A>(self: L.List<A>): L.List<A | B> => {
+    if (isNil(self)) {
+      return prefix
+    } else if (isNil(prefix)) {
+      return self
+    } else {
+      const result = new ConsImpl<A | B>(prefix.head, self)
+      let curr = result
+      let that = prefix.tail
+      while (!isNil(that)) {
+        const temp = new ConsImpl<A | B>(that.head, self)
+        curr.tail = temp
+        curr = temp
+        that = that.tail
+      }
+      return result
+    }
+  }
+}
+
+/** @internal */
+export function concat<B>(that: L.List<B>) {
+  return <A>(self: L.List<A>): L.List<A | B> => pipe(that, prependAll(self))
+}
+
+/** @internal */
+export function empty<A = never>(): L.List<A> {
+  return nil()
+}
+
+/** @internal */
+export function any<A>(p: Predicate<A>) {
+  return (self: L.List<A>): boolean => {
+    let these = self
+    while (!isNil(these)) {
+      if (p(these.head)) {
+        return true
+      }
+      these = these.tail
+    }
+    return false
+  }
+}
+
+/** @internal */
+export function filter<A, B extends A>(p: Refinement<A, B>): (self: L.List<A>) => L.List<B>
+/** @internal */
+export function filter<A>(p: Predicate<A>): (self: L.List<A>) => L.List<A>
+/** @internal */
+export function filter<A>(p: Predicate<A>) {
+  return (self: L.List<A>): L.List<A> => filterCommon_(self, p, false)
+}
+
+/** @internal */
+function noneIn<A>(l: L.List<A>, p: Predicate<A>, isFlipped: boolean): L.List<A> {
+  /* eslint-disable no-constant-condition */
+  while (true) {
+    if (isNil(l)) {
+      return nil()
+    } else {
+      if (p(l.head) !== isFlipped) {
+        return allIn(l, l.tail, p, isFlipped)
+      } else {
+        l = l.tail
+      }
+    }
+  }
+}
+
+/** @internal */
+function allIn<A>(
+  start: L.List<A>,
+  remaining: L.List<A>,
+  p: Predicate<A>,
+  isFlipped: boolean
+): L.List<A> {
+  /* eslint-disable no-constant-condition */
+  while (true) {
+    if (isNil(remaining)) {
+      return start
+    } else {
+      if (p(remaining.head) !== isFlipped) {
+        remaining = remaining.tail
+      } else {
+        return partialFill(start, remaining, p, isFlipped)
+      }
+    }
+  }
+}
+
+/** @internal */
+function partialFill<A>(
+  origStart: L.List<A>,
+  firstMiss: L.List<A>,
+  p: Predicate<A>,
+  isFlipped: boolean
+): L.List<A> {
+  const newHead = new ConsImpl<A>(unsafeHead(origStart)!, nil())
+  let toProcess = unsafeTail(origStart)! as L.Cons<A>
+  let currentLast = newHead
+
+  while (!(toProcess === firstMiss)) {
+    const newElem = new ConsImpl(unsafeHead(toProcess)!, nil())
+    currentLast.tail = newElem
+    currentLast = unsafeCoerce(newElem)
+    toProcess = unsafeCoerce(toProcess.tail)
+  }
+
+  let next = firstMiss.tail
+  let nextToCopy: L.Cons<A> = unsafeCoerce(next)
+  while (!isNil(next)) {
+    const head = unsafeHead(next)!
+    if (p(head) !== isFlipped) {
+      next = next.tail
+    } else {
+      while (!(nextToCopy === next)) {
+        const newElem = cons(unsafeHead(nextToCopy)!, nil())
+        currentLast.tail = newElem
+        currentLast = newElem
+        nextToCopy = unsafeCoerce(nextToCopy.tail)
+      }
+      nextToCopy = unsafeCoerce(next.tail)
+      next = next.tail
+    }
+  }
+
+  if (!isNil(nextToCopy)) {
+    currentLast.tail = nextToCopy
+  }
+
+  return newHead
+}
+
+/** @internal */
+function filterCommon_<A>(list: L.List<A>, p: Predicate<A>, isFlipped: boolean): L.List<A> {
+  return noneIn(list, p, isFlipped)
+}
+
+/** @internal */
+export function find<A, B extends A>(p: Refinement<A, B>): (self: L.List<A>) => O.Option<B>
+/** @internal */
+export function find<A>(p: Predicate<A>): (self: L.List<A>) => O.Option<A>
+/** @internal */
+export function find<A>(p: Predicate<A>) {
+  return (self: L.List<A>): O.Option<A> => {
+    let these = self
+    while (!isNil(these)) {
+      if (p(these.head)) {
+        return O.some(these.head)
+      }
+      these = these.tail
+    }
+    return O.none
+  }
+}
+
+/** @internal */
+export function flatMap<A, B>(f: (a: A) => L.List<B>) {
+  return (self: L.List<A>): L.List<B> => {
+    let rest = self
+    let h: ConsImpl<B> | undefined = undefined
+    let t: ConsImpl<B> | undefined = undefined
+    while (!isNil(rest)) {
+      let bs = f(rest.head)
+      while (!isNil(bs)) {
+        const nx = new ConsImpl(bs.head, nil())
+        if (t === undefined) {
+          h = nx
+        } else {
+          t.tail = nx
+        }
+        t = nx
+        bs = bs.tail
+      }
+      rest = rest.tail
+    }
+    if (h === undefined) return nil()
+    else return h
+  }
+}
+
+/** @internal */
+export function all<A>(f: Predicate<A>) {
+  return (self: L.List<A>): boolean => {
+    for (const a of self) {
+      if (!f(a)) {
+        return false
+      }
+    }
+    return true
+  }
+}
+
+/** @internal */
+export function forEach<A, U>(f: (a: A) => U) {
+  return (self: L.List<A>): void => {
+    let these = self
+    while (!isNil(these)) {
+      f(these.head)
+      these = these.tail
+    }
+  }
+}
+
+/** @internal */
+export function from<A>(prefix: Iterable<A>): L.List<A> {
+  const iter = prefix[Symbol.iterator]()
+  let a: IteratorResult<A>
+  if (!(a = iter.next()).done) {
+    const result = new ConsImpl(a.value, nil())
+    let curr = result
+    while (!(a = iter.next()).done) {
+      const temp = new ConsImpl(a.value, nil())
+      curr.tail = temp
+      curr = temp
+    }
+    return result
+  } else {
+    return nil()
+  }
+}
+
+/** @internal */
+export function make<As extends ReadonlyArray<any>>(...prefix: As): L.List<As[number]> {
+  return from(prefix)
+}
+
+/** @internal */
+export function head<A>(self: L.List<A>): O.Option<A> {
+  return isNil(self) ? O.none : O.some(self.head)
+}
+
+/** @internal */
+export function last<A>(self: L.List<A>): O.Option<A> {
+  return isNil(self) ? O.none : O.some(unsafeLast(self)!)
+}
+
+/** @internal */
+export function unsafeLast<A>(self: L.List<A>): A {
+  if (isNil(self)) {
+    throw new Error("Expected List to be not empty")
+  }
+  let these = self
+  let scout = self.tail
+  while (!isNil(scout)) {
+    these = scout
+    scout = scout.tail
+  }
+  return these.head
+}
+
+/** @internal */
+export function map<A, B>(f: (a: A) => B) {
+  return (self: L.List<A>): L.List<B> => {
+    if (isNil(self)) {
+      return self as unknown as L.List<B>
+    } else {
+      const h = new ConsImpl(f(self.head), nil())
+      let t = h
+      let rest = self.tail
+      while (!isNil(rest)) {
+        const nx = new ConsImpl(f(rest.head), nil())
+        t.tail = nx
+        t = nx
+        rest = rest.tail
+      }
+      return h
+    }
+  }
+}
+
+/** @internal */
+export function partition<A>(f: Predicate<A>) {
+  return (self: L.List<A>): readonly [L.List<A>, L.List<A>] => {
+    const left: Array<A> = []
+    const right: Array<A> = []
+    for (const a of self) {
+      if (f(a)) {
+        right.push(a)
+      } else {
+        left.push(a)
+      }
+    }
+    return [from(left), from(right)]
+  }
+}
+
+/** @internal */
+export function partitionMap<A, B, C>(f: (a: A) => R.Result<B, C>) {
+  return (self: L.List<A>): readonly [L.List<B>, L.List<C>] => {
+    const left: Array<B> = []
+    const right: Array<C> = []
+    for (const a of self) {
+      const e = f(a)
+      if (R.isFailure(e)) {
+        left.push(e.failure)
+      } else {
+        right.push(e.success)
+      }
+    }
+    return [from(left), from(right)]
+  }
+}
+
+/** @internal */
+export function prepend<B>(elem: B) {
+  return <A>(self: L.List<A>): L.Cons<A | B> => cons<A | B>(elem, self)
+}
+
+/** @internal */
+export function reverse<A>(self: L.List<A>): L.List<A> {
+  let result = empty<A>()
+  let these = self
+  while (!isNil(these)) {
+    result = pipe(result, prepend(these.head))
+    these = these.tail
+  }
+  return result
+}
+
+/** @internal */
+export function splitAt(n: number) {
+  return <A>(self: L.List<A>): readonly [L.List<A>, L.List<A>] => [take(n)(self), drop(n)(self)]
+}
+
+/** @internal */
+export function tail<A>(self: L.List<A>): O.Option<L.List<A>> {
+  return isNil(self) ? O.none : O.some(self.tail)
+}
+
+/** @internal */
+export function sortWith<A>(ord: Ord<A>) {
+  return (self: L.List<A>): L.List<A> => {
+    const len = length(self)
+    const b = new ListBuilder<A>()
+    if (len === 1) {
+      b.append(unsafeHead(self)!)
+    } else if (len > 1) {
+      const arr = new Array<[number, A]>(len)
+      copyToArrayWithIndex(self, arr)
+      arr.sort(([i, x], [j, y]) => {
+        const c = ord.compare(y)(x)
+        return c !== 0 ? c : i < j ? -1 : 1
+      })
+      for (let i = 0; i < len; i++) {
+        b.append(arr[i]![1])
+      }
+    }
+    return b.build()
+  }
+}
+
+/** @internal */
+function copyToArrayWithIndex<A>(list: L.List<A>, arr: Array<[number, A]>): void {
+  let these = list
+  let i = 0
+  while (!isNil(these)) {
+    arr[i] = [i, these.head]
+    these = these.tail
+    i++
+  }
+}
+
+/** @internal */
+export class ListBuilder<A> implements L.ListBuilder<A> {
+  private first: L.List<A> = nil()
+  private last0: ConsImpl<A> | undefined = undefined
+  private len = 0;
+
+  [Symbol.iterator](): Iterator<A> {
+    return this.first[Symbol.iterator]()
+  }
+
+  get length(): number {
+    return this.len
+  }
+
+  isEmpty = (): boolean => {
+    return this.len === 0
+  }
+
+  unsafeHead = (): A => {
+    if (this.isEmpty()) {
+      throw new Error("Expected ListBuilder to be not empty")
+    }
+    return (this.first as L.Cons<A>).head
+  }
+
+  unsafeTail = (): L.List<A> => {
+    if (this.isEmpty()) {
+      throw new Error("Expected ListBuilder to be not empty")
+    }
+    return (this.first as L.Cons<A>).tail
+  }
+
+  append = (elem: A): ListBuilder<A> => {
+    const last1 = new ConsImpl(elem, nil())
+    if (this.len === 0) {
+      this.first = last1
+    } else {
+      this.last0!.tail = last1
+    }
+    this.last0 = last1
+    this.len += 1
+    return this
+  }
+
+  prepend = (elem: A): ListBuilder<A> => {
+    this.insert(0, elem)
+    return this
+  }
+
+  unprepend = (): A => {
+    if (this.isEmpty()) {
+      throw new Error("no such element")
+    }
+    const h = (this.first as L.Cons<A>).head
+    this.first = (this.first as L.Cons<A>).tail
+    this.len -= 1
+    return h
+  }
+
+  build = (): L.List<A> => {
+    return this.first
+  }
+
+  insert = (idx: number, elem: A): ListBuilder<A> => {
+    if (idx < 0 || idx > this.len) {
+      throw new Error(`Index ${idx} out of bounds ${0} ${this.len - 1}`)
+    }
+    if (idx === this.len) {
+      this.append(elem)
+    } else {
+      const p = this.locate(idx)
+      const nx = cons(elem, this.getNext(p))
+      if (p === undefined) {
+        this.first = nx
+      } else {
+        ;(p as ConsImpl<A>).tail = nx
+      }
+      this.len += 1
+    }
+    return this
+  }
+
+  reduce = <B>(b: B, f: (b: B, a: A) => B): B => {
+    return reduce(b, f)(this.first)
+  }
+
+  private getNext(p: L.List<A> | undefined): L.List<A> {
+    if (p === undefined) {
+      return this.first
+    } else {
+      return unsafeTail(p)!
+    }
+  }
+
+  private locate(i: number): L.List<A> | undefined {
+    if (i === 0) {
+      return undefined
+    } else if (i === this.len) {
+      return this.last0
+    } else {
+      let p = this.first
+      for (let j = i - 1; j > 0; j--) {
+        p = unsafeTail(p)!
+      }
+      return p
+    }
+  }
+}
+
+/** @internal */
+export function builder<A>(): ListBuilder<A> {
+  return new ListBuilder()
+}
+
+/** @internal */
+export const Functor: _functor.Functor<L.ListTypeLambda> = {
+  map
+}
+
+/** @internal */
+export const of: <A>(a: A) => L.List<A> = (a) => cons(a, nil())
+
+/** @internal */
+export const FromIdentity: _fromIdentity.FromIdentity<L.ListTypeLambda> = {
+  of
+}
+
+/** @internal */
+export const ap: <A>(
+  fa: L.List<A>
+) => <B>(self: L.List<(a: A) => B>) => L.List<B> = (fa) =>
+  (self) => pipe(self, flatMap((f) => pipe(fa, map((a) => f(a)))))
+
+/** @internal */
+export const Apply: _apply.Apply<L.ListTypeLambda> = {
+  map,
+  ap
+}
