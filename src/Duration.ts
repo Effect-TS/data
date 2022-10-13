cda30fbda3a43baadbb1dabd316839c3e5c85923
0x706b <peter@peterkrol.com>
2022-10-13 13:29:13 -0400

    feat: add Duration

---
+++ src/Duration.ts
@@ -0,0 +1,140 @@
+import * as DE from "@fp-ts/data/DeepEqual"
+import * as DH from "@fp-ts/data/DeepHash"
+
+const TypeId: unique symbol = Symbol.for("@fp-ts/data/Duration")
+
+/**
+ * @since 1.0.0
+ * @category symbol
+ */
+export type TypeId = typeof TypeId
+
+/**
+ * @since 1.0.0
+ * @category model
+ */
+export interface Duration {
+  readonly _id: TypeId
+  readonly millis: number
+}
+
+/** @internal */
+export class DurationImpl implements DE.DeepEqual {
+  readonly _id: TypeId = TypeId
+  constructor(readonly millis: number) {}
+  [DH.DeepHash.symbol](): number {
+    return DH.deepHash(this.millis)
+  }
+  [DE.DeepEqual.symbol](that: unknown): boolean {
+    return isDuration(that) && this.millis === that.millis
+  }
+}
+
+/**
+ * @since 1.0.0
+ * @category guards
+ */
+export const isDuration = (u: unknown): u is Duration =>
+  typeof u === "object" && u != null && "_id" in u && u["_id"] === TypeId
+
+/**
+ * @since 1.0.0
+ * @category constructors
+ */
+export const zero: Duration = new DurationImpl(0)
+
+/**
+ * @since 1.0.0
+ * @category constructors
+ */
+export const infinity: Duration = new DurationImpl(Infinity)
+
+/**
+ * @since 1.0.0
+ * @category constructors
+ */
+export const millis = (millis: number): Duration => new DurationImpl(millis)
+
+/**
+ * @since 1.0.0
+ * @category constructors
+ */
+export const seconds = (seconds: number): Duration => new DurationImpl(seconds * 1000)
+
+/**
+ * @since 1.0.0
+ * @category constructors
+ */
+export const minutes = (minutes: number): Duration => new DurationImpl(minutes * 60_000)
+
+/**
+ * @since 1.0.0
+ * @category constructors
+ */
+export const hours = (hours: number): Duration => new DurationImpl(hours * 3_600_000)
+
+/**
+ * @since 1.0.0
+ * @category constructors
+ */
+export const days = (days: number): Duration => new DurationImpl(days * 86_400_000)
+
+/**
+ * @since 1.0.0
+ * @category constructors
+ */
+export const weeks = (weeks: number): Duration => new DurationImpl(weeks * 604_800_000)
+
+/**
+ * @since 1.0.0
+ * @category mutations
+ */
+export const times = (times: number) =>
+  (self: Duration): Duration => new DurationImpl(self.millis * times)
+
+/**
+ * @since 1.0.0
+ * @category mutations
+ */
+export const add = (that: Duration) =>
+  (self: Duration): Duration => new DurationImpl(self.millis + that.millis)
+
+/**
+ * @since 1.0.0
+ * @category mutations
+ */
+export const subtract = (that: Duration) =>
+  (self: Duration): Duration => new DurationImpl(self.millis - that.millis)
+
+/**
+ * @since 1.0.0
+ * @category comparisons
+ */
+export const lessThan = (that: Duration) => (self: Duration): boolean => self.millis < that.millis
+
+/**
+ * @since 1.0.0
+ * @category comparisons
+ */
+export const lessThanOrEqualTo = (that: Duration) =>
+  (self: Duration): boolean => self.millis <= that.millis
+
+/**
+ * @since 1.0.0
+ * @category comparisons
+ */
+export const greaterThan = (that: Duration) =>
+  (self: Duration): boolean => self.millis > that.millis
+
+/**
+ * @since 1.0.0
+ * @category comparisons
+ */
+export const greaterThanOrEqualTo = (that: Duration) =>
+  (self: Duration): boolean => self.millis >= that.millis
+
+/**
+ * @since 1.0.0
+ * @category comparisons
+ */
+export const equals = (that: Duration) => (self: Duration): boolean => self.millis === that.millis
---
+++ test/Duration.ts
@@ -0,0 +1,38 @@
+import { pipe } from "@fp-ts/core/Function"
+import * as D from "@fp-ts/data/Duration"
+
+describe.concurrent("Duration", () => {
+  it("equals", () => {
+    assert.isTrue(pipe(D.hours(1), D.equals(D.minutes(60))))
+  })
+  it("times", () => {
+    assert.isTrue(
+      pipe(D.minutes(1), D.equals(pipe(D.seconds(1), D.times(60))))
+    )
+  })
+  it("add", () => {
+    assert.isTrue(
+      pipe(D.minutes(1), D.equals(pipe(D.seconds(30), D.add(D.seconds(30)))))
+    )
+  })
+  it(">", () => {
+    assert.isTrue(pipe(D.seconds(30), D.greaterThan(D.seconds(20))))
+    assert.isFalse(pipe(D.seconds(30), D.greaterThan(D.seconds(30))))
+    assert.isFalse(pipe(D.seconds(30), D.greaterThan(D.minutes(1))))
+  })
+  it(">=", () => {
+    assert.isTrue(pipe(D.seconds(30), D.greaterThanOrEqualTo(D.seconds(20))))
+    assert.isTrue(pipe(D.seconds(30), D.greaterThanOrEqualTo(D.seconds(30))))
+    assert.isFalse(pipe(D.seconds(30), D.greaterThanOrEqualTo(D.minutes(1))))
+  })
+  it("<", () => {
+    assert.isTrue(pipe(D.seconds(20), D.lessThan(D.seconds(30))))
+    assert.isFalse(pipe(D.seconds(30), D.lessThan(D.seconds(30))))
+    assert.isFalse(pipe(D.minutes(1), D.lessThan(D.seconds(30))))
+  })
+  it("<=", () => {
+    assert.isTrue(pipe(D.seconds(20), D.lessThanOrEqualTo(D.seconds(30))))
+    assert.isTrue(pipe(D.seconds(30), D.lessThanOrEqualTo(D.seconds(30))))
+    assert.isFalse(pipe(D.minutes(1), D.lessThanOrEqualTo(D.seconds(30))))
+  })
+})
