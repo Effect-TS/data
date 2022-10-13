add582f1ae54fb500286fde04069420636ad8738
0x706b <peter@peterkrol.com>
2022-10-13 13:29:13 -0400

    feat: add Duration

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as chunk from "@fp-ts/data/Chunk"
 import * as context from "@fp-ts/data/Context"
 import * as deepEqual from "@fp-ts/data/DeepEqual"
 import * as deepHash from "@fp-ts/data/DeepHash"
+import * as duration from "@fp-ts/data/Duration"
 import * as list from "@fp-ts/data/List"
 import * as mutableHashMap from "@fp-ts/data/MutableHashMap"
 import * as mutableListBuilder from "@fp-ts/data/MutableListBuilder"
@@ -27,6 +28,10 @@ export {
    * @since 1.0.0
    */
   deepHash,
+  /**
+   * @since 1.0.0
+   */
+  duration,
   /**
    * @since 1.0.0
    */
