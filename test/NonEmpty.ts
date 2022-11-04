c7adc7cdc1d99c4bb2b17f961ddd2307118dc6df
Michael Arnaldi <arnaldimichael@gmail.com>
2022-11-04 11:26:42 +0000

    NonEmpty: add NonEmptyIterable

---
+++ test/NonEmpty.ts
@@ -0,0 +1,19 @@
+import * as Chunk from "@fp-ts/data/Chunk"
+import * as NonEmpty from "@fp-ts/data/NonEmpty"
+
+describe("NonEmpty", () => {
+  it("should get head and rest", () => {
+    const [head, rest] = NonEmpty.unprepend(Chunk.make(0, 1, 2))
+    const restArray: Array<number> = []
+    let next = rest.next()
+    while (!next.done) {
+      restArray.push(next.value)
+      next = rest.next()
+    }
+    expect(head).toEqual(0)
+    expect(restArray).toEqual([1, 2])
+  })
+  it("should throw", () => {
+    expect(() => NonEmpty.unprepend(Chunk.empty as any)).toThrow()
+  })
+})
