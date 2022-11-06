6dd81ab4723bac170c4c16f978d85b2bbb833850
gcanti <giulio.canti@gmail.com>
2022-11-06 06:54:18 +0100

    move Const, Endomorphism modules to limbo

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 export default defineConfig({
   test: {
     include: ["./test/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    exclude: ["./test/**/util.ts"],
+    exclude: ["./test/**/util.ts", "./test/limbo"],
     globals: true
   },
   resolve: {
