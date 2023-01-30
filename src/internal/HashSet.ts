188305e4c5be22e00ed64837539608d5d8fb2b74
Maxwell Brown <maxwellbrown1990@gmail.com>
2023-01-29 22:20:42 -0500

    convert HashSet to dual

--- src/internal/HashSet.ts
+++ src/internal/HashSet.ts
@@ -1,4 +1,5 @@
 import type { Predicate, Refinement } from "@fp-ts/core/Predicate"
+import * as Dual from "@fp-ts/data/Dual"
 import * as Equal from "@fp-ts/data/Equal"
 import * as Hash from "@fp-ts/data/Hash"
 import type { HashMap } from "@fp-ts/data/HashMap"
@@ -49,251 +50,264 @@ export class HashSetImpl<A> implements HS.HashSet<A> {
 }
 
 /** @internal */
-export function isHashSet<A>(u: Iterable<A>): u is HS.HashSet<A>
-export function isHashSet(u: unknown): u is HS.HashSet<unknown>
-export function isHashSet(u: unknown): u is HS.HashSet<unknown> {
-  return typeof u === "object" && u != null && "_id" in u && u["_id"] === HashSetTypeId
-}
+export const isHashSet: {
+  <A>(u: Iterable<A>): u is HS.HashSet<A>
+  (u: unknown): u is HS.HashSet<unknown>
+} = (u: unknown): u is HS.HashSet<unknown> =>
+  typeof u === "object" && u != null && "_id" in u && u["_id"] === HashSetTypeId
 
 /** @internal */
-export function empty<A = never>(): HS.HashSet<A> {
-  return new HashSetImpl(HM.empty<A, unknown>())
-}
+export const empty = <A = never>(): HS.HashSet<A> => new HashSetImpl(HM.empty<A, unknown>())
 
 /** @internal */
-export function from<A>(elements: Iterable<A>): HS.HashSet<A> {
+export const from = <A>(elements: Iterable<A>): HS.HashSet<A> => {
   const set = beginMutation(empty<A>())
   for (const value of elements) {
-    add(value)(set)
+    add(set, value)
   }
   return endMutation(set)
 }
 
 /** @internal */
-export function make<As extends ReadonlyArray<any>>(...elements: As): HS.HashSet<As[number]> {
+export const make = <As extends ReadonlyArray<any>>(...elements: As): HS.HashSet<As[number]> => {
   const set = beginMutation(empty<As[number]>())
   for (const value of elements) {
-    add(value)(set)
+    add(set, value)
   }
   return endMutation(set)
 }
 
 /** @internal */
-export function has<A>(value: A) {
-  return (self: HS.HashSet<A>): boolean => {
-    return HM.has(value)((self as HashSetImpl<A>)._keyMap)
-  }
-}
+export const has = Dual.dual<
+  <A>(self: HS.HashSet<A>, value: A) => boolean,
+  <A>(value: A) => (self: HS.HashSet<A>) => boolean
+>(2, <A>(self: HS.HashSet<A>, value: A) => HM.has((self as HashSetImpl<A>)._keyMap, value))
 
 /** @internal */
-export function some<A>(f: Predicate<A>) {
-  return (self: HS.HashSet<A>): boolean => {
-    let found = false
-    for (const value of self) {
-      found = f(value)
-      if (found) {
-        break
-      }
+export const some = Dual.dual<
+  <A>(self: HS.HashSet<A>, f: Predicate<A>) => boolean,
+  <A>(f: Predicate<A>) => (self: HS.HashSet<A>) => boolean
+>(2, (self, f) => {
+  let found = false
+  for (const value of self) {
+    found = f(value)
+    if (found) {
+      break
     }
-    return found
   }
-}
+  return found
+})
 
 /** @internal */
-export function every<A>(f: Predicate<A>) {
-  return (self: HS.HashSet<A>): boolean => {
-    return !some<A>((a) => !f(a))(self)
-  }
-}
+export const every = Dual.dual<
+  <A>(self: HS.HashSet<A>, f: Predicate<A>) => boolean,
+  <A>(f: Predicate<A>) => (self: HS.HashSet<A>) => boolean
+>(2, (self, f) => !some(self, (a) => !f(a)))
 
 /** @internal */
-export function isSubset<A>(that: HS.HashSet<A>) {
-  return (self: HS.HashSet<A>): boolean => {
-    return every<A>((value) => has(value)(that))(self)
-  }
-}
+export const isSubset = Dual.dual<
+  <A>(self: HS.HashSet<A>, that: HS.HashSet<A>) => boolean,
+  <A>(that: HS.HashSet<A>) => (self: HS.HashSet<A>) => boolean
+>(2, (self, that) => every(self, (value) => has(that, value)))
 
 /** @internal */
-export function values<A>(self: HS.HashSet<A>): IterableIterator<A> {
-  return HM.keys((self as HashSetImpl<A>)._keyMap)
-}
+export const values = <A>(self: HS.HashSet<A>): IterableIterator<A> =>
+  HM.keys((self as HashSetImpl<A>)._keyMap)
 
 /** @internal */
-export function size<A>(self: HS.HashSet<A>): number {
-  return HM.size((self as HashSetImpl<A>)._keyMap)
-}
+export const size = <A>(self: HS.HashSet<A>): number => HM.size((self as HashSetImpl<A>)._keyMap)
 
 /** @internal */
-export function beginMutation<A>(self: HS.HashSet<A>): HS.HashSet<A> {
-  return new HashSetImpl(HM.beginMutation((self as HashSetImpl<A>)._keyMap))
-}
+export const beginMutation = <A>(self: HS.HashSet<A>): HS.HashSet<A> =>
+  new HashSetImpl(HM.beginMutation((self as HashSetImpl<A>)._keyMap))
 
 /** @internal */
-export function endMutation<A>(self: HS.HashSet<A>): HS.HashSet<A> {
+export const endMutation = <A>(self: HS.HashSet<A>): HS.HashSet<A> => {
   ;((self as HashSetImpl<A>)._keyMap as HM.HashMapImpl<A, unknown>)._editable = false
   return self
 }
 
 /** @internal */
-export function mutate<A>(f: (set: HS.HashSet<A>) => void) {
-  return (self: HS.HashSet<A>): HS.HashSet<A> => {
-    const transient = beginMutation(self)
-    f(transient)
-    return endMutation(transient)
-  }
-}
+export const mutate = Dual.dual<
+  <A>(self: HS.HashSet<A>, f: (set: HS.HashSet<A>) => void) => HS.HashSet<A>,
+  <A>(f: (set: HS.HashSet<A>) => void) => (self: HS.HashSet<A>) => HS.HashSet<A>
+>(2, (self, f) => {
+  const transient = beginMutation(self)
+  f(transient)
+  return endMutation(transient)
+})
 
 /** @internal */
-export function add<A>(value: A) {
-  return (self: HS.HashSet<A>): HS.HashSet<A> => {
-    return ((self as HashSetImpl<A>)._keyMap as HM.HashMapImpl<A, unknown>)._editable
+export const add = Dual.dual<
+  <A>(self: HS.HashSet<A>, value: A) => HS.HashSet<A>,
+  <A>(value: A) => (self: HS.HashSet<A>) => HS.HashSet<A>
+>(
+  2,
+  <A>(self: HS.HashSet<A>, value: A) =>
+    ((self as HashSetImpl<A>)._keyMap as HM.HashMapImpl<A, unknown>)._editable
       ? (HM.set(value as A, true as unknown)((self as HashSetImpl<A>)._keyMap), self)
       : new HashSetImpl(HM.set(value as A, true as unknown)((self as HashSetImpl<A>)._keyMap))
-  }
-}
+)
 
 /** @internal */
-export function remove<A>(value: A) {
-  return (self: HS.HashSet<A>): HS.HashSet<A> => {
-    return (((self as HashSetImpl<A>)._keyMap) as HM.HashMapImpl<A, unknown>)._editable
+export const remove = Dual.dual<
+  <A>(self: HS.HashSet<A>, value: A) => HS.HashSet<A>,
+  <A>(value: A) => (self: HS.HashSet<A>) => HS.HashSet<A>
+>(
+  2,
+  <A>(self: HS.HashSet<A>, value: A) =>
+    (((self as HashSetImpl<A>)._keyMap) as HM.HashMapImpl<A, unknown>)._editable
       ? (HM.remove(value)((self as HashSetImpl<A>)._keyMap), self)
       : new HashSetImpl(HM.remove(value)((self as HashSetImpl<A>)._keyMap))
-  }
-}
+)
 
 /** @internal */
-export function difference<A>(that: Iterable<A>) {
-  return (self: HS.HashSet<A>): HS.HashSet<A> => {
-    return mutate<A>((set) => {
-      for (const value of that) {
-        remove(value)(set)
-      }
-    })(self)
-  }
-}
+export const difference = Dual.dual<
+  <A>(self: HS.HashSet<A>, that: Iterable<A>) => HS.HashSet<A>,
+  <A>(that: Iterable<A>) => (self: HS.HashSet<A>) => HS.HashSet<A>
+>(2, (self, that) =>
+  mutate(self, (set) => {
+    for (const value of that) {
+      remove(set, value)
+    }
+  }))
 
 /** @internal */
-export function intersection<A>(that: Iterable<A>) {
-  return (self: HS.HashSet<A>): HS.HashSet<A> => {
-    return mutate<A>((set) => {
-      for (const value of that) {
-        if (has(value)(self)) {
-          add(value)(set)
-        }
-      }
-    })(empty<A>())
-  }
-}
-
-/** @internal */
-export function union<A>(that: Iterable<A>) {
-  return (self: HS.HashSet<A>): HS.HashSet<A> => {
-    return mutate<A>((set) => {
-      forEach((value) => {
-        add(value)(set)
-      })(self)
-      for (const value of that) {
+export const intersection = Dual.dual<
+  <A>(self: HS.HashSet<A>, that: Iterable<A>) => HS.HashSet<A>,
+  <A>(that: Iterable<A>) => (self: HS.HashSet<A>) => HS.HashSet<A>
+>(2, (self, that) =>
+  mutate(empty(), (set) => {
+    for (const value of that) {
+      if (has(value)(self)) {
         add(value)(set)
       }
-    })(empty<A>())
-  }
-}
+    }
+  }))
 
 /** @internal */
-export function toggle<A>(value: A) {
-  return (self: HS.HashSet<A>): HS.HashSet<A> => {
-    return has(value)(self) ? remove(value)(self) : add(value)(self)
-  }
-}
+export const union = Dual.dual<
+  <A>(self: HS.HashSet<A>, that: Iterable<A>) => HS.HashSet<A>,
+  <A>(that: Iterable<A>) => (self: HS.HashSet<A>) => HS.HashSet<A>
+>(2, (self, that) =>
+  mutate(empty(), (set) => {
+    forEach(self, (value) => add(set, value))
+    for (const value of that) {
+      add(set, value)
+    }
+  }))
 
 /** @internal */
-export function map<A, B>(f: (a: A) => B) {
-  return (self: HS.HashSet<A>): HS.HashSet<B> => {
-    return mutate<B>((set) => {
-      forEach<A>((a) => {
-        const b = f(a)
-        if (!has(b)(set)) {
-          add(b)(set)
-        }
-      })(self)
-    })(empty<B>())
-  }
-}
+export const toggle = Dual.dual<
+  <A>(self: HS.HashSet<A>, value: A) => HS.HashSet<A>,
+  <A>(value: A) => (self: HS.HashSet<A>) => HS.HashSet<A>
+>(2, (self, value) => has(self, value) ? remove(self, value) : add(self, value))
 
 /** @internal */
-export function flatMap<A, B>(f: (a: A) => Iterable<B>) {
-  return (self: HS.HashSet<A>): HS.HashSet<B> => {
-    return mutate<B>((set) => {
-      forEach<A>((a) => {
-        for (const b of f(a)) {
-          if (!has(b)(set)) {
-            add(b)(set)
-          }
-        }
-      })(self)
-    })(empty<B>())
-  }
-}
+export const map = Dual.dual<
+  <A, B>(self: HS.HashSet<A>, f: (a: A) => B) => HS.HashSet<B>,
+  <A, B>(f: (a: A) => B) => (self: HS.HashSet<A>) => HS.HashSet<B>
+>(2, (self, f) =>
+  mutate(empty(), (set) => {
+    forEach(self, (a) => {
+      const b = f(a)
+      if (!has(set, b)) {
+        add(set, b)
+      }
+    })
+  }))
 
 /** @internal */
-export function forEach<A>(f: (value: A) => void) {
-  return (self: HS.HashSet<A>): void => {
-    HM.forEachWithIndex<unknown, A>((_, k) => {
-      f(k)
-    })((self as HashSetImpl<A>)._keyMap)
-  }
-}
-
-/** @internal */
-export function reduce<A, Z>(zero: Z, f: (accumulator: Z, value: A) => Z) {
-  return (self: HS.HashSet<A>): Z => {
-    return HM.reduceWithIndex<Z, unknown, A>(zero, (z, _, a) => f(z, a))(
-      (self as HashSetImpl<A>)._keyMap
-    )
-  }
-}
-
-/** @internal */
-export function filter<A, B extends A>(f: Refinement<A, B>): (self: HS.HashSet<A>) => HS.HashSet<B>
-export function filter<A>(f: Predicate<A>): (self: HS.HashSet<A>) => HS.HashSet<A>
-export function filter<A>(f: Predicate<A>) {
-  return (self: HS.HashSet<A>): HS.HashSet<A> => {
-    return mutate<A>((set) => {
-      const iterator = values(self)
-      let next: IteratorResult<A, any>
-      while (!(next = iterator.next()).done) {
-        const value = next.value
-        if (f(value)) {
-          add(value)(set)
+export const flatMap = Dual.dual<
+  <A, B>(self: HS.HashSet<A>, f: (a: A) => Iterable<B>) => HS.HashSet<B>,
+  <A, B>(f: (a: A) => Iterable<B>) => (self: HS.HashSet<A>) => HS.HashSet<B>
+>(2, (self, f) =>
+  mutate(empty(), (set) => {
+    forEach(self, (a) => {
+      for (const b of f(a)) {
+        if (!has(set, b)) {
+          add(set, b)
         }
       }
-    })(empty<A>())
-  }
-}
+    })
+  }))
 
 /** @internal */
-export function partition<A, B extends A>(
-  f: Refinement<A, B>
-): (self: HS.HashSet<A>) => readonly [HS.HashSet<A>, HS.HashSet<B>]
-export function partition<A>(
-  f: Predicate<A>
-): (self: HS.HashSet<A>) => readonly [HS.HashSet<A>, HS.HashSet<A>]
-export function partition<A>(
-  f: Predicate<A>
-) {
-  return (self: HS.HashSet<A>): readonly [HS.HashSet<A>, HS.HashSet<A>] => {
+export const forEach = Dual.dual<
+  <A>(self: HS.HashSet<A>, f: (value: A) => void) => void,
+  <A>(f: (value: A) => void) => (self: HS.HashSet<A>) => void
+>(2, <A>(self: HS.HashSet<A>, f: (value: A) => void) =>
+  HM.forEachWithIndex(
+    (self as HashSetImpl<A>)._keyMap,
+    (_, k) => f(k)
+  ))
+
+/** @internal */
+export const reduce = Dual.dual<
+  <A, Z>(self: HS.HashSet<A>, zero: Z, f: (accumulator: Z, value: A) => Z) => Z,
+  <A, Z>(zero: Z, f: (accumulator: Z, value: A) => Z) => (self: HS.HashSet<A>) => Z
+>(3, <A, Z>(self: HS.HashSet<A>, zero: Z, f: (accumulator: Z, value: A) => Z) =>
+  HM.reduceWithIndex(
+    (self as HashSetImpl<A>)._keyMap,
+    zero,
+    (z, _, a) => f(z, a)
+  ))
+
+/** @internal */
+export const filter = Dual.dual<
+  {
+    <A, B extends A>(self: HS.HashSet<A>, f: Refinement<A, B>): HS.HashSet<B>
+    <A>(self: HS.HashSet<A>, f: Predicate<A>): HS.HashSet<A>
+  },
+  {
+    <A, B extends A>(f: Refinement<A, B>): (self: HS.HashSet<A>) => HS.HashSet<B>
+    <A>(f: Predicate<A>): (self: HS.HashSet<A>) => HS.HashSet<A>
+  }
+>(2, <A>(self: HS.HashSet<A>, f: Predicate<A>) => {
+  return mutate(empty(), (set) => {
     const iterator = values(self)
     let next: IteratorResult<A, any>
-    const right = beginMutation(empty<A>())
-    const left = beginMutation(empty<A>())
     while (!(next = iterator.next()).done) {
       const value = next.value
       if (f(value)) {
-        add(value)(right)
-      } else {
-        add(value)(left)
+        add(set, value)
       }
     }
-    return [endMutation(left), endMutation(right)]
+  })
+})
+
+/** @internal */
+export const partition = Dual.dual<
+  {
+    <A, B extends A>(
+      self: HS.HashSet<A>,
+      f: Refinement<A, B>
+    ): readonly [HS.HashSet<A>, HS.HashSet<B>]
+    <A>(
+      self: HS.HashSet<A>,
+      f: Predicate<A>
+    ): readonly [HS.HashSet<A>, HS.HashSet<A>]
+  },
+  {
+    <A, B extends A>(
+      f: Refinement<A, B>
+    ): (self: HS.HashSet<A>) => readonly [HS.HashSet<A>, HS.HashSet<B>]
+    <A>(
+      f: Predicate<A>
+    ): (self: HS.HashSet<A>) => readonly [HS.HashSet<A>, HS.HashSet<A>]
   }
-}
+>(2, <A>(self: HS.HashSet<A>, f: Predicate<A>) => {
+  const iterator = values(self)
+  let next: IteratorResult<A, any>
+  const right = beginMutation(empty<A>())
+  const left = beginMutation(empty<A>())
+  while (!(next = iterator.next()).done) {
+    const value = next.value
+    if (f(value)) {
+      add(right, value)
+    } else {
+      add(left, value)
+    }
+  }
+  return [endMutation(left), endMutation(right)] as const
+})
