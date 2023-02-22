8cf419608fd92b8bda426df676349186d6baf5b9
Patrick Roza <contact@patrickroza.com>
2023-02-22 11:43:31 +0100

    Replace symbol name in test

--- test/Symbol.ts
+++ test/Symbol.ts
@@ -2,7 +2,7 @@ import * as S from "@effect/data/Symbol"
 
 describe.concurrent("Symbol", () => {
   it("isSymbol", () => {
-    expect(S.isSymbol(Symbol.for("@fp-ts/core/test/a"))).toEqual(true)
+    expect(S.isSymbol(Symbol.for("@effect/data/test/a"))).toEqual(true)
     expect(S.isSymbol(1n)).toEqual(false)
     expect(S.isSymbol(1)).toEqual(false)
     expect(S.isSymbol("a")).toEqual(false)
@@ -11,7 +11,7 @@ describe.concurrent("Symbol", () => {
 
   it("Equivalence", () => {
     const eq = S.Equivalence
-    expect(eq(Symbol.for("@fp-ts/core/test/a"), Symbol.for("@fp-ts/core/test/a"))).toBe(true)
-    expect(eq(Symbol.for("@fp-ts/core/test/a"), Symbol.for("@fp-ts/core/test/b"))).toBe(false)
+    expect(eq(Symbol.for("@effect/data/test/a"), Symbol.for("@effect/data/test/a"))).toBe(true)
+    expect(eq(Symbol.for("@effect/data/test/a"), Symbol.for("@effect/data/test/b"))).toBe(false)
   })
 })
