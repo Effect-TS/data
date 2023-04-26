d04aecc51c3e91758dabf8fa429ee15f53680064
Giulio Canti <giulio.canti@gmail.com>
2023-04-26 14:55:00 +0200

    fix failing type-level tests (#343)

--- dtslint/ts4.9/Unify.ts
+++ dtslint/ts4.9/Unify.ts
@@ -2,17 +2,17 @@ import * as Option from '@effect/data/Option'
 import * as Either from '@effect/data/Either'
 import * as Unify from '@effect/data/Unify'
 
-// $ExpectType Option.Option<string | number>
-declare const option: Unify.Unify<Option.Option<number> | Option.Option<string>>
+// $ExpectType Option<string | number>
+type option = Unify.Unify<Option.Option<number> | Option.Option<string>>
 
-// $ExpectType Either.Either<"LA" | "LB", "RA" | "RB">
-declare const either: Unify.Unify<Either.Either<"LA", "RA"> | Either.Either<"LB", "RB">>
+// $ExpectType Either<"LA" | "LB", "RA" | "RB">
+type either = Unify.Unify<Either.Either<"LA", "RA"> | Either.Either<"LB", "RB">>
 
-// $ExpectType 0 | Option.Option<string | number> | Either.Either<"LA" | "LB", "RA" | "RB">
-declare const both: Unify.Unify<Either.Either<"LA", "RA"> | Either.Either<"LB", "RB"> | Option.Option<number> | Option.Option<string> | 0>
+// $ExpectType 0 | Option<string | number> | Either<"LA" | "LB", "RA" | "RB">
+type both = Unify.Unify<Either.Either<"LA", "RA"> | Either.Either<"LB", "RB"> | Option.Option<number> | Option.Option<string> | 0>
 
-// $ExpectType <N>(n: N) => Either.Either<string, N>
+// $ExpectType <N>(n: N) => Either<string, N>
 const b = Unify.unify(<N>(n: N) => Math.random() > 0 ? Either.right(n) : Either.left("ok"))
 
-// $ExpectType Either.Either<string, number>
+// $ExpectType Either<string, number>
 const c = Unify.unify(Math.random() > 0 ? Either.right(10) : Either.left("ok"))
