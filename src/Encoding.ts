50ed550d0c92f2778a4c4ef2dcb568647dd70267
Sebastian Lorenz <fubhy@fubhy.com>
2023-08-22 14:47:25 +0200

    add base64url

--- src/Encoding.ts
+++ src/Encoding.ts
@@ -2,11 +2,13 @@
  * This module provides encoding & decoding functionality for:
  *
  * - base64 (RFC4648)
+ * - base64 (URL)
  *
  * @since 1.0.0
  */
 import * as Either from "@effect/data/Either"
 import * as Base64 from "@effect/data/internal/Encoding/Base64"
+import * as Base64Url from "@effect/data/internal/Encoding/Base64Url"
 
 /**
  * Encodes a Uint8Array into a base64 (RFC4648) string.
@@ -39,10 +41,49 @@ export const decodeBase64 = (str: string): Either.Either<Base64DecodeError, Uint
 }
 
 /**
- * Unsafely decodes a base64 (RFC4648) encoded string unsafely. Throws an error if the
+ * Unsafely decodes a base64 (RFC4648) encoded string. Throws a type error if the
  * given value isn't a valid base64 (RFC4648) string.
  *
  * @category encoding
  * @since 1.0.0
  */
 export const unsafeDecodeBase64 = Base64.decode
+
+/**
+ * Encodes a Uint8Array into a base64 (URL) string.
+ *
+ * @category encoding
+ * @since 1.0.0
+ */
+export const encodeBase64Url = Base64Url.encode
+
+/**
+ * @since 1.0.0
+ */
+export class Base64UrlDecodeError {
+  /** @since 1.0.0 */
+  readonly _tag = "Base64UrlDecodeError"
+}
+
+/**
+ * Decodes a base64 (URL) encoded string.
+ *
+ * @category encoding
+ * @since 1.0.0
+ */
+export const decodeBase64Url = (str: string): Either.Either<Base64UrlDecodeError, Uint8Array> => {
+  try {
+    return Either.right(Base64Url.decode(str))
+  } catch {
+    return Either.left(new Base64UrlDecodeError())
+  }
+}
+
+/**
+ * Unsafely decodes a base64 (URL) encoded string. Throws a type error if the
+ * given value isn't a valid base64 (URL) string.
+ *
+ * @category encoding
+ * @since 1.0.0
+ */
+export const unsafeDecodeBase64Url = Base64Url.decode
--- test/Encoding.ts
+++ test/Encoding.ts
@@ -52,3 +52,42 @@ describe.concurrent("Base64", () => {
     deepStrictEqual(decoded.left, new Encoding.Base64DecodeError())
   })
 })
+
+describe.concurrent("Base64Url", () => {
+  const valid: Array<[string, string]> = [
+    ["", ""],
+    ["ß", "w58"],
+    ["f", "Zg"],
+    ["fo", "Zm8"],
+    ["foo", "Zm9v"],
+    ["foob", "Zm9vYg"],
+    ["fooba", "Zm9vYmE"],
+    ["foobar", "Zm9vYmFy"],
+    [">?>d?ß", "Pj8-ZD_Dnw"]
+  ]
+
+  const invalid: Array<string> = [
+    "Pj8/ZD+Dnw",
+    "PDw/Pz8+Pg",
+    "Pj8/ZD+Dnw==",
+    "PDw/Pz8+Pg=="
+  ]
+
+  it.each(valid)(`should decode %j => %j`, (raw: string, encoded: string) => {
+    const bytes = new TextEncoder().encode(raw)
+    const decoded = Encoding.decodeBase64Url(encoded)
+    assert(Either.isRight(decoded))
+    deepStrictEqual(decoded.right, bytes)
+  })
+
+  it.each(valid)(`should encode %j <= %j`, (raw: string, encoded: string) => {
+    const bytes = new TextEncoder().encode(raw)
+    strictEqual(Encoding.encodeBase64Url(bytes), encoded)
+  })
+
+  it.each(invalid)(`should refuse to decode %j`, (encoded: string) => {
+    const decoded = Encoding.decodeBase64Url(encoded)
+    assert(Either.isLeft(decoded))
+    deepStrictEqual(decoded.left, new Encoding.Base64UrlDecodeError())
+  })
+})
