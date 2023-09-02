fb0d15cf18beefd1a35e52ff965d86a51ab7cf13
Sebastian Lorenz <fubhy@fubhy.com>
2023-09-02 14:18:13 +0200

    add `string` encoding & decoding variants

--- src/Encoding.ts
+++ src/Encoding.ts
@@ -7,14 +7,14 @@
  *
  * @since 1.0.0
  */
-import type * as Either from "@effect/data/Either"
+import * as Either from "@effect/data/Either"
 import * as Base64 from "@effect/data/internal/Encoding/Base64"
 import * as Base64Url from "@effect/data/internal/Encoding/Base64Url"
-import * as common from "@effect/data/internal/Encoding/Common"
+import * as Common from "@effect/data/internal/Encoding/Common"
 import * as Hex from "@effect/data/internal/Encoding/Hex"
 
 /**
- * Encodes a Uint8Array into a base64 (RFC4648) string.
+ * Encodes a `Uint8Array` into a base64 (RFC4648) `string`.
  *
  * @category encoding
  * @since 1.0.0
@@ -22,7 +22,15 @@ import * as Hex from "@effect/data/internal/Encoding/Hex"
 export const encodeBase64: (bytes: Uint8Array) => string = Base64.encode
 
 /**
- * Decodes a base64 (RFC4648) encoded string.
+ * Encodes a `string` into a base64 (RFC4648) `string`.
+ *
+ * @category encoding
+ * @since 1.0.0
+ */
+export const encodeBase64String = (str: string) => encodeBase64(Common.encoder.encode(str))
+
+/**
+ * Decodes a base64 (RFC4648) encoded `string` into a `Uint8Array`.
  *
  * @category decoding
  * @since 1.0.0
@@ -30,7 +38,15 @@ export const encodeBase64: (bytes: Uint8Array) => string = Base64.encode
 export const decodeBase64 = (str: string): Either.Either<DecodeException, Uint8Array> => Base64.decode(str)
 
 /**
- * Encodes a Uint8Array into a base64 (URL) string.
+ * Decodes a base64 (RFC4648) encoded `string` into a UTF-8 `string`.
+ *
+ * @category decoding
+ * @since 1.0.0
+ */
+export const decodeBase64String = (str: string) => Either.map(decodeBase64(str), (_) => Common.decoder.decode(_))
+
+/**
+ * Encodes a `Uint8Array` into a base64 (URL) `string`.
  *
  * @category encoding
  * @since 1.0.0
@@ -38,7 +54,15 @@ export const decodeBase64 = (str: string): Either.Either<DecodeException, Uint8A
 export const encodeBase64Url: (bytes: Uint8Array) => string = Base64Url.encode
 
 /**
- * Decodes a base64 (URL) encoded string.
+ * Encodes a `string` into a base64 (URL) `string`.
+ *
+ * @category encoding
+ * @since 1.0.0
+ */
+export const encodeBase64UrlString = (str: string) => encodeBase64Url(Common.encoder.encode(str))
+
+/**
+ * Decodes a base64 (URL) encoded `string` into a `Uint8Array`.
  *
  * @category decoding
  * @since 1.0.0
@@ -46,7 +70,15 @@ export const encodeBase64Url: (bytes: Uint8Array) => string = Base64Url.encode
 export const decodeBase64Url = (str: string): Either.Either<DecodeException, Uint8Array> => Base64Url.decode(str)
 
 /**
- * Encodes a Uint8Array into a hex string.
+ * Decodes a base64 (URL) encoded `string` into a UTF-8 `string`.
+ *
+ * @category decoding
+ * @since 1.0.0
+ */
+export const decodeBase64UrlString = (str: string) => Either.map(decodeBase64Url(str), (_) => Common.decoder.decode(_))
+
+/**
+ * Encodes a `Uint8Array` into a hex `string`.
  *
  * @category encoding
  * @since 1.0.0
@@ -54,7 +86,15 @@ export const decodeBase64Url = (str: string): Either.Either<DecodeException, Uin
 export const encodeHex: (bytes: Uint8Array) => string = Hex.encode
 
 /**
- * Decodes a hex encoded string.
+ * Encodes a `string` into a hex `string`.
+ *
+ * @category encoding
+ * @since 1.0.0
+ */
+export const encodeHexString = (str: string) => encodeHex(Common.encoder.encode(str))
+
+/**
+ * Decodes a hex encoded `string` into a `Uint8Array`.
  *
  * @category decoding
  * @since 1.0.0
@@ -62,10 +102,18 @@ export const encodeHex: (bytes: Uint8Array) => string = Hex.encode
 export const decodeHex = (str: string): Either.Either<DecodeException, Uint8Array> => Hex.decode(str)
 
 /**
+ * Decodes a hex encoded `string` into a UTF-8 `string`.
+ *
+ * @category decoding
+ * @since 1.0.0
+ */
+export const decodeHexString = (str: string) => Either.map(decodeHex(str), (_) => Common.decoder.decode(_))
+
+/**
  * @since 1.0.0
  * @category symbols
  */
-export const DecodeExceptionTypeId: unique symbol = common.DecodeExceptionTypeId
+export const DecodeExceptionTypeId: unique symbol = Common.DecodeExceptionTypeId
 
 /**
  * @since 1.0.0
@@ -92,7 +140,7 @@ export interface DecodeException {
  * @since 1.0.0
  * @category errors
  */
-export const DecodeException: (input: string, message?: string) => DecodeException = common.DecodeException
+export const DecodeException: (input: string, message?: string) => DecodeException = Common.DecodeException
 
 /**
  * Returns `true` if the specified value is an `DecodeException`, `false` otherwise.
@@ -100,4 +148,4 @@ export const DecodeException: (input: string, message?: string) => DecodeExcepti
  * @since 1.0.0
  * @category refinements
  */
-export const isDecodeException: (u: unknown) => u is DecodeException = common.isDecodeException
+export const isDecodeException: (u: unknown) => u is DecodeException = Common.isDecodeException
